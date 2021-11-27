470bfc146e5d8b4fd9bce5908608126af9a6c5fc
Levente Murgas <murgaslevi@gmail.com>
2021-11-27 15:38:17 +0100

    A játék fullscreen modeban indul

---
+++ src/nhf/AmobaMain.java
@@ -0,0 +1,12 @@
+package nhf;
+
+import javax.swing.*;
+
+public class AmobaMain {
+
+    public static void main(String[] args){
+        AmobaFrame af = new AmobaFrame();
+        af.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        af.setVisible(true);
+    }
+}
