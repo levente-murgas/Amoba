2c9e9ca76081f0ed294c8f8fa12b59f0253c42e6
Levente Murgas <murgaslevi@gmail.com>
2021-11-27 10:21:33 +0100

    még 2 test

--- test/nhf/LoadPanelTest.java
+++ test/nhf/LoadPanelTest.java
@@ -1,4 +1,29 @@
 package nhf;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class LoadPanelTest {
+    LoadPanel lp;
+
+    @Before
+    public void init(){
+        lp = new LoadPanel();
+    }
+
+    @Test
+    public void loadGameTest(){
+        String res = lp.loadGame(1,"\\test\\testsave");
+        Assert.assertEquals("""
+                            OXO0OX0O0O00XOO
+                            0XO0XOO0XXX00XO
+                            OX0X0000000000X
+                            3""",res);
+    }
+
+    @Test
+    public void setUpGameTest(){
+
+    }
 }
